64660581762cd1926ac65341c134bc7025e022df
萬俊瑋 <wei840222@gmail.com>
2018-12-07 23:30:43 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+(async function () {
+    const axios = require('axios')
+
+    const {
+        PLUGIN_PROJECT_API,
+        PLUGIN_ACCESS_KEY,
+        PLUGIN_SECRET_KEY,
+        PLUGIN_DEPLOY_IMAGE
+    } = process.env
+    
+    const workloadInfo = await axios.get(PLUGIN_PROJECT_API, {
+        auth: {
+            username: PLUGIN_ACCESS_KEY,
+            password: PLUGIN_SECRET_KEY
+        }
+    })
+
+    workloadInfo.data.containers.forEach(element => element.image = PLUGIN_DEPLOY_IMAGE)
+
+    const workloadInfoNew = await axios.put(PLUGIN_PROJECT_API, workloadInfo.data, {
+        auth: {
+            username: PLUGIN_ACCESS_KEY,
+            password: PLUGIN_SECRET_KEY
+        }
+    })
+
+    console.log(workloadInfoNew.data)
+})()
