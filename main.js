1ec98b184429fa696d563fe3d4dc437997a84bfc
wei840222 <wei840222@gmail.com>
2019-07-18 21:46:38 +0800

    add try catch

--- main.js
+++ main.js
@@ -7,22 +7,23 @@
         PLUGIN_SECRET_KEY,
         PLUGIN_DEPLOY_IMAGE
     } = process.env
-    
-    const workloadInfo = await axios.get(PLUGIN_PROJECT_API, {
-        auth: {
-            username: PLUGIN_ACCESS_KEY,
-            password: PLUGIN_SECRET_KEY
-        }
-    })
 
-    workloadInfo.data.containers.forEach(element => element.image = PLUGIN_DEPLOY_IMAGE)
-
-    const workloadInfoNew = await axios.put(PLUGIN_PROJECT_API, workloadInfo.data, {
-        auth: {
-            username: PLUGIN_ACCESS_KEY,
-            password: PLUGIN_SECRET_KEY
-        }
-    })
-
-    console.log(workloadInfoNew.data)
+    try {
+        const workloadInfo = await axios.get(PLUGIN_PROJECT_API, {
+            auth: {
+                username: PLUGIN_ACCESS_KEY,
+                password: PLUGIN_SECRET_KEY
+            }
+        })
+        workloadInfo.data.containers.forEach(element => element.image = PLUGIN_DEPLOY_IMAGE)
+        const workloadInfoNew = await axios.put(PLUGIN_PROJECT_API, workloadInfo.data, {
+            auth: {
+                username: PLUGIN_ACCESS_KEY,
+                password: PLUGIN_SECRET_KEY
+            }
+        })
+        console.log(workloadInfoNew.data)
+    } catch (err) {
+        console.error(err)
+    }
 })()
